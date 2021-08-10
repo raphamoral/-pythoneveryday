bf99ef390027400c751e7f28f68357f1e9b82797
Raphael Moral <80533929+raphamoral@users.noreply.github.com>
2021-08-10 18:52:03 -0300

    Add files via upload

---
+++ Estrutura sequencial pythonBrasil/PythonBrasil Exercicio 5.py
@@ -0,0 +1,6 @@
+##5.Faça um Programa que converta metros para centímetros.
+##make a program that converts meters to centimeters
+
+a= (float(input(" Insira um valor para converter metros em centimetros : ")))
+b= a*100
+print("O valor em centimetros é :" +str(b))
