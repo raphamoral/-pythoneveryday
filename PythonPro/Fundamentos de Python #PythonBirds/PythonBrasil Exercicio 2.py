4f79e73aab306f9fd7a1825785313435e221d826
raphamoral <80533929+raphamoral@users.noreply.github.com>
2021-08-10 00:43:27 -0300

    Add files via upload

---
+++ PythonPro/Fundamentos de Python #PythonBirds/PythonBrasil Exercicio 2.py
@@ -0,0 +1,5 @@
+##Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+##Make a Program that asks for a number and then displays the message The number entered was [number].
+
+number= float(input('Digite um número'))
+print(number)
